304476fde68db9016c00b242a95c6ea9eeaf9131
unknown <musyabiqbal12@gmail.com>
2024-10-26 16:01:42 +0600

    work on class

---
+++ class5/video23/index.js
@@ -0,0 +1,7 @@
+// for(var sum = 0; sum <= 5; sum++){
+// console.log(sum)
+// }
+var Nums = [0,1,2,3,4,5,6,7,8,9, "eshan", "ahme","ahad"];
+for(var i =0; i < Nums.length; i ++){
+  console.log(Nums[i])
+}
---
+++ class5/video25/index.js
@@ -0,0 +1,13 @@
+// function Fun(){
+//     var num = 50;
+//     var num2 = 50;
+//     console.log(num * num2);
+// }
+// Fun()
+// console.log("Shakwat")
+function fun(num1){
+    var total = num1 * 10;
+    return total;
+}
+var a = fun(10);
+console.log(a)
